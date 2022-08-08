7d3fefa5f91cad3f3a0dccf23bf9e7cf8f968cb1
killer-cf <costa.kilder@live.com>
2022-08-08 15:51:30 -0300

    seed of products

--- spec/system/product/user_view_products_spec.rb
+++ spec/system/product/user_view_products_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe 'visitor sees all products' do
   it 'from initial screen' do
-    product1 = create :product
-    product2 = create :product
+    product1 = create :product, price: 200
+    product2 = create :product, price: 300
 
     visit root_path
 
     expect(page).to have_content product1.name
     expect(page).to have_content product1.brand
-    expect(page).to have_content product1.price
+    expect(page).to have_content 'R$ 200,00'
     expect(page).to have_content product2.name
     expect(page).to have_content product2.brand
-    expect(page).to have_content product2.price
+    expect(page).to have_content 'R$ 300,00'
   end
 
   it 'and theres no products' do
