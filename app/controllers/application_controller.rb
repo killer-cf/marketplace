021b476e1e127d3eb3bbb816254ed490b6db0162
killer-cf <costa.kilder@live.com>
2022-08-10 23:35:11 -0300

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  def after_sign_in_path_for(resource)
-    '/'
-  end
-
-  def after_sign_out_path_for(scope)
-    '/'
-  end
 end
