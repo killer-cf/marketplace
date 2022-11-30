3cba954f819bdc356c0d4b0ff561aeb76f3a02fc
killer-cf <costa.kilder@live.com>
2022-11-30 09:10:33 -0300

    att server deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-role :app, %w{deploy@191.101.71.97}
-role :web, %w{deploy@191.101.71.97}
-role :db, %w{deploy@191.101.71.97}
+role :app, %w{deploy@192.168.1.55}
+role :web, %w{deploy@192.168.1.55}
+role :db, %w{deploy@192.168.1.55}
 
 # role-based syntax
 # ==================
